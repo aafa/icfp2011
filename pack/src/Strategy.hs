d98952f6e1ba944f3094f26fa4a1e49f8103b83c
Alexey Afanasev <avafanasiev@gmail.com>
2011-06-20 03:31:59 +0400

    final release; readme added;

--- pack/src/Strategy.hs
+++ pack/src/Strategy.hs
@@ -1,33 +1,38 @@
 module Strategy where
 import Helpers
 
+-- main help iSlot jSlot elephant
+eSlot = 1 -- an elephant slot (used to attack)
+zSlot = 4 -- zombie slot
+tSlot = 2 -- template storage slot (n; n+1)
+iSlot = 5 
+jSlot = 6 -- contains 255 in the end of first wave
 
 -- 10000 int function
 makeElephant host = let h = show host in [(right, "zero", h)] ++ times [(left, "succ", h)] 39 ++ times [(left, "dbl", h)] 8 ++ times [(left, "succ", h)] 16
-
 -- 11112 int function
 makeKiller host inBetween = let h = show host in [(right, "zero", h)] ++ times [(left, "succ", h)] 5 ++ times [(left, "dbl", h)] 8 ++ inBetween ++ times [(left, "succ", h)] 109 ++ times [(left, "dbl", h)] 3
 
--- take number from slot #1
--- makeHelp target hero -- is a generic
-
+-- basic helpers
 noop = [(left, "I", "0")]
 clean host = let h = show host in [(left, "put", h)]
 incValueNumber h n = [(right, "zero", h)] ++ times [(left, "succ", h)] n
 operationTarget target h = times [(left, "K", h), (left, "S", h), (right, "succ", h)] target
-copyToBaseFrom n = clean 0 ++ incValueNumber "0" n ++ [(left, "get", "0")]
+copyTo target src = incValueNumber (show target) src ++ [(left, "get", show target)]
+copyToBaseFrom n = clean 0 ++ copyTo 0 n
 
 -- simple combos
 make operation target hero host = let h = show host in incValueNumber h hero ++[(left, operation, h)] ++ operationTarget target h ++ [(right, "zero", h)]
-addThirdParam host = let h = show host in [(left, "K", h), (left, "S", h), (right, "get", h), (left, "K", h), (left, "S", h), (right, "succ", h), (right, "zero", h)]
-simpleAttackOn target hero =  make "attack" target hero 0 ++ addThirdParam 0
-simpleHeal target hero = make "help" target hero 0 ++ addThirdParam 0
+addThirdParam host slot = let h = show host in [(left, "K", h), (left, "S", h), (right, "get", h)] ++ times [(left, "K", h), (left, "S", h), (right, "succ", h)] slot ++ [(right, "zero", h)]
+addThirdParam1 host = let h = show host in [(left, "K", h), (left, "S", h), (right, "succ", h), (right, "zero", h)]
+simpleAttackOn target hero =  make "attack" target hero 0 ++ addThirdParam 0 1
+simpleHeal target hero = make "help" target hero 0 ++ addThirdParam 0 1
 strike n host target = let h = show host in times ([(right, "zero", h)] ++ times [(left, "succ", h)] target ++ [(left, "dec", h)]) n 
 
 -- Zombie stuff (ZF = Zombie Function)
-makeZombie host = let h = show host in [(right, "zero", h), (left, "zombie", h), (left, "K", h) , (left, "S", h), (right, "get", h), (right, "zero", h)]
+makeZombie host = let h = show host in [(right, "zero", h), (left, "dec", h)] ++ [(right, "zero", h), (left, "zombie", h), (left, "K", h) , (left, "S", h), (right, "get", h), (right, "zero", h)]
 updateZF = [(left, "K", "0"), (left, "S", "0"), (left, "K", "0"), (left, "S", "0"), (right, "get", "0"), (left, "K", "0"), (left, "S", "0"), (right, "succ", "0"), (right, "zero", "0")]
-makeZF = clean 1 ++ makeElephant 1 ++ [(left, "K", "1")]
+makeZF = clean eSlot ++ makeElephant eSlot ++ [(left, "K", show eSlot)]
 makeFastZF = helpTemplate tSlot ++ copyToBaseFrom tSlot
 
 -- template (n,n+1) to make fast "help" to opponent
@@ -41,28 +46,32 @@ helpTemplate host = let h = show host in formTemplatePart (host-1) (operationFol
 
 -- template (i,j)
 ig slot host = let h = show host in g h slot ++ copyToBaseFrom host ++ clean host ++ [(right, "K", h), (left, "K", h),(left, "S", h), (right, "get", h),(right, "zero", h)]
-neutralTemplate host func = ig 3 host ++ func ++ clean 0 ++ clean host ++ ig 4 host
-makeNeutralTemplate host = let h = show host in neutralTemplate (host-1) (operationFold "help" host) ++ [(left, "S", h), (left, "K", h), (left, "S", h), (right, "get", h), (right, "zero", h)] 
+neutralTemplate host func = ig iSlot host ++ func ++ clean 0 ++ clean host ++ ig jSlot host
+makeNeutralTemplate host = let h = show host in neutralTemplate (host+1) (operationFold "help" host) ++ [(left, "S", h), (left, "K", h), (left, "S", h), (right, "get", h), (right, "zero", h)] 
 
 -- template helpers
-tSlot = 6 -- temple storage slot
-iSlot = 3 -- help iSlot jSlot elephant
-jSlot = 4
-flushTemplate = copyToBaseFrom tSlot ++ [(right, "I", "0")] ++ updateZF
+flushTemplate = copyToBaseFrom tSlot ++ [(right, "I", "0")] ++ copyTo 7 0 ++ updateZF
 updateCounter n = times [(left, "succ", show iSlot)] n ++ times [(left, "succ", show jSlot)] n
 initCounters = [(right, "zero", show iSlot), (right, "zero", show jSlot)] ++ times [(left, "succ", show iSlot)] 10
 getJSlotUpToTen = times [(left, "succ", show jSlot)] 11
 resetCounters = clean iSlot ++ clean jSlot ++ initCounters
 
+-- defend: (close 255-th; randomly heal)
+blindHeal n 	| n < 100 =  addThirdParam1 7
+		| otherwise = copyTo 8 iSlot ++ [(left, "revive", show 8)]
+reviveLast =  [(left, "revive", show jSlot)] ++ [(left, "revive", show iSlot)] -- when jSlot=255 iSlot=254
+
 -- macro combos
 -- 1Kill  (158 turns)
 firstKill = makeKiller 1 (simpleHeal 2 3) ++ simpleAttackOn 0 2
 overKill = clean tSlot ++ makeNeutralTemplate tSlot ++ clean 3 ++ initCounters ++ times [(left, "succ", "3")] 2 ++ flushTemplate ++ makeZombie 2
-zombiWaves = concat [flushTemplate ++ updateCounter 1 ++ makeZombie 2 | n <- [1..10]] ++ getJSlotUpToTen ++ concat [flushTemplate ++ updateCounter 2 ++ makeZombie 2 | n <- [1..117]] ++ flushTemplate ++ makeZombie 2 ++ firstKill ++ resetCounters ++ zombiWaves
+zombiWaves = concat [flushTemplate ++ updateCounter 1 ++ makeZombie zSlot | n <- [1..10]] ++ getJSlotUpToTen ++ concat [flushTemplate ++ updateCounter 2 ++ makeZombie zSlot ++ blindHeal n | n <- [1..117]] ++ flushTemplate ++ reviveLast ++ makeZombie zSlot ++ firstKill ++ resetCounters ++ zombiWaves
 
 -- stratages
-fastAttack = attack 0 where attack n = simpleAttackOn n (n+2) ++ strike 1001 0 n ++ attack (n+1)
-zombiAttack = makeZF ++ makeNeutralTemplate tSlot ++ initCounters ++ flushTemplate ++ makeZombie 2 ++ zombiWaves
+simpleAttack = attack 0 where attack n = simpleAttackOn n (n+2) ++ strike 1001 0 n ++ attack (n+1)
+zombiAttack = makeZF ++ makeNeutralTemplate tSlot ++ initCounters ++ zombiWaves
 
+-- final list
 listOfTurns =  firstKill ++ zombiAttack
+
 
--- src/Strategy.hs
+++ src/Strategy.hs
@@ -1,16 +1,19 @@
 module Strategy where
 import Helpers
 
+-- main help iSlot jSlot elephant
+eSlot = 1 -- an elephant slot (used to attack)
+zSlot = 4 -- zombie slot
+tSlot = 2 -- template storage slot (n; n+1)
+iSlot = 5 
+jSlot = 6 -- contains 255 in the end of first wave
 
 -- 10000 int function
 makeElephant host = let h = show host in [(right, "zero", h)] ++ times [(left, "succ", h)] 39 ++ times [(left, "dbl", h)] 8 ++ times [(left, "succ", h)] 16
-
 -- 11112 int function
 makeKiller host inBetween = let h = show host in [(right, "zero", h)] ++ times [(left, "succ", h)] 5 ++ times [(left, "dbl", h)] 8 ++ inBetween ++ times [(left, "succ", h)] 109 ++ times [(left, "dbl", h)] 3
 
--- take number from slot #1
--- makeHelp target hero -- is a generic
-
+-- basic helpers
 noop = [(left, "I", "0")]
 clean host = let h = show host in [(left, "put", h)]
 incValueNumber h n = [(right, "zero", h)] ++ times [(left, "succ", h)] n
@@ -27,9 +30,9 @@ simpleHeal target hero = make "help" target hero 0 ++ addThirdParam 0 1
 strike n host target = let h = show host in times ([(right, "zero", h)] ++ times [(left, "succ", h)] target ++ [(left, "dec", h)]) n 
 
 -- Zombie stuff (ZF = Zombie Function)
-makeZombie host = let h = show host in [(right, "zero", h), (left, "zombie", h), (left, "K", h) , (left, "S", h), (right, "get", h), (right, "zero", h)]
+makeZombie host = let h = show host in [(right, "zero", h), (left, "dec", h)] ++ [(right, "zero", h), (left, "zombie", h), (left, "K", h) , (left, "S", h), (right, "get", h), (right, "zero", h)]
 updateZF = [(left, "K", "0"), (left, "S", "0"), (left, "K", "0"), (left, "S", "0"), (right, "get", "0"), (left, "K", "0"), (left, "S", "0"), (right, "succ", "0"), (right, "zero", "0")]
-makeZF = clean 1 ++ makeElephant 1 ++ [(left, "K", "1")]
+makeZF = clean eSlot ++ makeElephant eSlot ++ [(left, "K", show eSlot)]
 makeFastZF = helpTemplate tSlot ++ copyToBaseFrom tSlot
 
 -- template (n,n+1) to make fast "help" to opponent
@@ -43,13 +46,10 @@ helpTemplate host = let h = show host in formTemplatePart (host-1) (operationFol
 
 -- template (i,j)
 ig slot host = let h = show host in g h slot ++ copyToBaseFrom host ++ clean host ++ [(right, "K", h), (left, "K", h),(left, "S", h), (right, "get", h),(right, "zero", h)]
-neutralTemplate host func = ig 3 host ++ func ++ clean 0 ++ clean host ++ ig 4 host
+neutralTemplate host func = ig iSlot host ++ func ++ clean 0 ++ clean host ++ ig jSlot host
 makeNeutralTemplate host = let h = show host in neutralTemplate (host+1) (operationFold "help" host) ++ [(left, "S", h), (left, "K", h), (left, "S", h), (right, "get", h), (right, "zero", h)] 
 
 -- template helpers
-tSlot = 5 -- temple storage slot
-iSlot = 3 -- help iSlot jSlot elephant
-jSlot = 4
 flushTemplate = copyToBaseFrom tSlot ++ [(right, "I", "0")] ++ copyTo 7 0 ++ updateZF
 updateCounter n = times [(left, "succ", show iSlot)] n ++ times [(left, "succ", show jSlot)] n
 initCounters = [(right, "zero", show iSlot), (right, "zero", show jSlot)] ++ times [(left, "succ", show iSlot)] 10
@@ -57,20 +57,21 @@ getJSlotUpToTen = times [(left, "succ", show jSlot)] 11
 resetCounters = clean iSlot ++ clean jSlot ++ initCounters
 
 -- defend: (close 255-th; randomly heal)
-messHeal =  addThirdParam1 7
+blindHeal n 	| n < 100 =  addThirdParam1 7
+		| otherwise = copyTo 8 iSlot ++ [(left, "revive", show 8)]
+reviveLast =  [(left, "revive", show jSlot)] ++ [(left, "revive", show iSlot)] -- when jSlot=255 iSlot=254
 
 -- macro combos
 -- 1Kill  (158 turns)
-
 firstKill = makeKiller 1 (simpleHeal 2 3) ++ simpleAttackOn 0 2
 overKill = clean tSlot ++ makeNeutralTemplate tSlot ++ clean 3 ++ initCounters ++ times [(left, "succ", "3")] 2 ++ flushTemplate ++ makeZombie 2
-zombiWaves = concat [flushTemplate ++ updateCounter 1 ++ makeZombie 2 | n <- [1..10]] ++ getJSlotUpToTen ++ concat [flushTemplate ++ updateCounter 2 ++ makeZombie 2 ++ messHeal| n <- [1..117]] ++ flushTemplate ++ makeZombie 2 ++ firstKill ++ resetCounters ++ zombiWaves
+zombiWaves = concat [flushTemplate ++ updateCounter 1 ++ makeZombie zSlot | n <- [1..10]] ++ getJSlotUpToTen ++ concat [flushTemplate ++ updateCounter 2 ++ makeZombie zSlot ++ blindHeal n | n <- [1..117]] ++ flushTemplate ++ reviveLast ++ makeZombie zSlot ++ firstKill ++ resetCounters ++ zombiWaves
 
 -- stratages
-fastAttack = attack 0 where attack n = simpleAttackOn n (n+2) ++ strike 1001 0 n ++ attack (n+1)
+simpleAttack = attack 0 where attack n = simpleAttackOn n (n+2) ++ strike 1001 0 n ++ attack (n+1)
 zombiAttack = makeZF ++ makeNeutralTemplate tSlot ++ initCounters ++ zombiWaves
 
+-- final list
 listOfTurns =  firstKill ++ zombiAttack
---listOfTurns = makeNeutralTemplate tSlot ++ initCounters ++ copyToBaseFrom tSlot ++ [(right, "I", "0")] ++ messHeal
 
 
